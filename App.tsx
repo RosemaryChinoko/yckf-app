9433bdbdadb692553296c402f473cc4dfc0cecc0
Peter Chinoko <52344650+Chinokoo@users.noreply.github.com>
2025-10-02 21:37:06 +0200

    set up the environment and created the apps splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/yckf-logo.png")} style={styles.logo} />
+      <Text style={styles.title}>Welcome to YCKF APP</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#3A86FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    resizeMode: "contain",
+  },
+  title: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
